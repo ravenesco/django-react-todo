4a7f9fe275f045cad46ebf74084b5bf32145b753
Efim Klimov <efim.klimov.95@gmail.com>
2022-03-31 17:20:11 +0300

    Fix static files MIME types issue

--- todo/views.py
+++ todo/views.py
@@ -2,8 +2,23 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import TodoSerializer
 from .models import Todo
+from django.views import View
+from django.http import HttpResponse, HttpResponseNotFound
+import os
 
 
 class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
+
+
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
